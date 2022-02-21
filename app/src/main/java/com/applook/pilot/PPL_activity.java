367d914ae37a79b76dbab6c1a80f3d71f5b32bab
0631155020 <54396606+0631155020@users.noreply.github.com>
2022-02-21 17:44:34 +0200

    Initial commit

---
+++ app/src/main/java/com/applook/pilot/PPL_activity.java
@@ -0,0 +1,14 @@
+package com.applook.pilot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PPL_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ppl);
+    }
+}
