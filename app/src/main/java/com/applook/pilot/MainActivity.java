d185ad4c6c2930a45dd5d327e0ee7139e1acd80a
0631155020 <54396606+0631155020@users.noreply.github.com>
2022-03-14 18:14:59 +0200

    l

--- app/src/main/java/com/applook/pilot/MainActivity.java
+++ app/src/main/java/com/applook/pilot/MainActivity.java
@@ -5,21 +5,23 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private long backPressedTime;
+    private Toast backToast;
     CardView PPl_button;
-    Button CPL_button;
+    CardView CPL_button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PPl_button = findViewById(R.id.button);
-        CPL_button = findViewById(R.id.button2);
+        PPl_button = findViewById(R.id.test_btn);
+        CPL_button = findViewById(R.id.random_btn);
 
 
         PPl_button.setOnClickListener(v -> {
@@ -27,13 +29,31 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        CPL_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CPL.class);
-                startActivity(intent);
-            }
+        CPL_button.setOnClickListener(v -> {
+
+            Intent intent = new Intent(getApplicationContext(), CPL.class);
+            startActivity(intent);
+
         });
 
     }
+// защита от случайного касания(двойное нажате на кнопку назад)
+
+    @Override
+    public void onBackPressed() {
+        backToast.cancel();
+        super.onBackPressed();
+
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Нажмите еще раз чтобы выйти", Toast.LENGTH_LONG);
+            backToast.show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+
+
 }
--- app/src/main/java/com/applook/pilot/law.java
+++ app/src/main/java/com/applook/pilot/law.java
@@ -2,7 +2,12 @@ package com.applook.pilot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class law extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class law extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_law);
+        final ImageView image_left = (ImageView) findViewById(R.id.image_left);
+//скругляем углы левой
+        image_left.setClipToOutline(true);
+
+        final ImageView image_right = (ImageView) findViewById(R.id.image_right);
+//скругляем углы правой
+        image_right.setClipToOutline(true);
+
+        // кнопка назад
+        Button btn_back = (Button)findViewById(R.id.button_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //обрабативаем нажатие
+                try {
+                    //вернутся назад
+                    Intent intent = new Intent(law.this,PPLActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
+        
     }
 }
